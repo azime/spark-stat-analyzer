859087f0cb85f672ad999746d23d0fabf75f4200
Vincent Lepot <vincent.lepot@canaltp.fr>
2015-12-15 10:42:14 +0100

    Uniformize with requests_calls

--- coverage_journeys.py
+++ coverage_journeys.py
@@ -1,24 +1,25 @@
 import sys
-from datetime import datetime
+from datetime import datetime, timedelta
 import json
 import os
 import shutil
 from pyspark import SparkContext, SparkConf
+from glob import glob
 
 
-def get_journey_count_from_stat_dict(statDict):
+def get_journey_count_from_stat_dict(stat_dict):
     result = []
-    if len(statDict['coverages']) > 0:
-        for cov in statDict['coverages']:
-            if 'region_id' in cov: # The empty coverages key is represented by [{}]
+    if len(stat_dict['coverages']) > 0:
+        for cov in stat_dict['coverages']:
+            if 'region_id' in cov:  # The empty coverages key is represented by [{}]
                 result.append(
                     (
                         (
-                            datetime.utcfromtimestamp(statDict['request_date']).date(),  # request_date
+                            datetime.utcfromtimestamp(stat_dict['request_date']).date(),  # request_date
                             cov['region_id'],  # region_id
-                            1 if 'canaltp.fr' in statDict['user_name'] else 0,  # is_internal_call
+                            1 if 'canaltp.fr' in stat_dict['user_name'] else 0,  # is_internal_call
                         ),
-                        len(statDict['journeys']) if 'journeys' in statDict else 0,  # nb_journeys
+                        len(stat_dict['journeys']) if 'journeys' in stat_dict else 0,  # nb_journeys
                     )
                 )
     return result
@@ -26,20 +27,31 @@ def get_journey_count_from_stat_dict(statDict):
 
 if __name__ == "__main__":
     if len(sys.argv) < 3:
-        raise SystemExit("Missing arguments. Usage: " + sys.argv[0] + " <date> <source_root>")
+        raise SystemExit("Missing arguments. Usage: " + sys.argv[0] + " <source_root> <start_date> <end_date> ")
+
+    # treatment_day = datetime.strptime(sys.argv[1], '%Y-%m-%d').date()
+    # source_root = '/home/vlepot/dev/navitia-stat-logger/tmp'
+    # source_root = 'gs://hdp_test'
 
     source_root = sys.argv[1]
-    treatment_day = datetime.strptime(sys.argv[2], '%Y-%m-%d').date()
-    #source_root = '/home/vlepot/dev/navitia-stat-logger/tmp'
-    #source_root = 'gs://hdp_test'
+    treatment_day_start = datetime.strptime(sys.argv[2], '%Y-%m-%d').date()
+    treatment_day_end = datetime.strptime(sys.argv[3], '%Y-%m-%d').date()
 
-    print "Go for date: " + treatment_day.strftime('%Y-%m-%d')
+    print "Go for dates: " + treatment_day_start.strftime('%Y-%m-%d') + " -> " + treatment_day_end.strftime('%Y-%m-%d')
     print "Source root dir: " + source_root
 
     conf = SparkConf().setAppName("coverage_journeys_compiler")
     sc = SparkContext(conf=conf)
 
-    statsLines = sc.textFile(source_root + '/' + treatment_day.strftime('%Y/%m/%d') + '/*')
+    statsLines = sc.emptyRDD()
+    treatment_day = treatment_day_start
+    while treatment_day <= treatment_day_end:
+        if source_root.startswith("/") and \
+                        len(glob(source_root + '/' + treatment_day.strftime('%Y/%m/%d') + '/*.json.log*')) > 0:
+            statsLines = statsLines.union(sc.textFile(
+                source_root + '/' + treatment_day.strftime('%Y/%m/%d') + '/*.json.log*')
+            )
+        treatment_day += timedelta(days=1)
 
     dayStats = statsLines.map(
         lambda stat: json.loads(stat)
@@ -59,16 +71,16 @@ if __name__ == "__main__":
 
     # Store on FS
 
-    compiled_storage_rootdir = source_root + "/compiled/" + treatment_day.strftime('%Y/%m/%d')
-    compiled_storage_dir = compiled_storage_rootdir + "/coverage_journeys_" + treatment_day.strftime('%Y%m%d')
+    compiled_storage_rootdir = source_root + "/compiled/" + treatment_day_start.strftime('%Y/%m/%d')
+    compiled_storage_dir = compiled_storage_rootdir + "/coverage_journeys_" + treatment_day_start.strftime('%Y%m%d')
 
-    if source_root.startswith('/'): # In case of local file system do some preparation first
+    if source_root.startswith('/'):  # In case of local file system do some preparation first
         if os.path.isdir(compiled_storage_dir):
             shutil.rmtree(compiled_storage_dir)
         else:
             if not os.path.isdir(compiled_storage_rootdir):
                 os.makedirs(compiled_storage_rootdir)
-    elif source_root.startswith('gs://'): # In case of Google Cloud Storage do some preparation first
+    elif source_root.startswith('gs://'):  # In case of Google Cloud Storage do some preparation first
         pass
 
     coverage_journeys.saveAsTextFile(compiled_storage_dir)
