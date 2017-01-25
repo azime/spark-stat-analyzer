4ff6ad2de73da8914ef96d54aef33a74cd266719
Nicolas Berard <nicolas.berard@canaltp.fr>
2017-01-25 16:39:45 +0100

    implement users table

---
+++ includes/common.py
@@ -0,0 +1,77 @@
+import sys
+from datetime import datetime, timedelta
+from pyspark import SparkContext, SparkConf
+from glob import glob
+from time import time
+import math
+import json
+import psycopg2
+
+def get_data_from_input(analyzer_name):
+    if len(sys.argv) < 3:
+        raise SystemExit("Missing arguments. Usage: " + sys.argv[0] + " <source_root> <start_date> <end_date> ")
+
+    source_root = sys.argv[1]
+    treatment_day_start = datetime.strptime(sys.argv[2], '%Y-%m-%d').date()
+    treatment_day_end = datetime.strptime(sys.argv[3], '%Y-%m-%d').date()
+
+    if treatment_day_start > treatment_day_end:
+        raise RuntimeError("<start_date> should be less than or equal to <end_date>")
+
+    print "Go for dates: " + treatment_day_start.strftime('%Y-%m-%d') + " -> " + treatment_day_end.strftime('%Y-%m-%d')
+    print "Source root dir: " + source_root
+
+    conf = SparkConf().setAppName(analyzer_name + "_compiler")
+    sc = SparkContext(conf=conf)
+
+    statsLines = sc.emptyRDD()
+    treatment_day = treatment_day_start
+    while treatment_day <= treatment_day_end:
+        if source_root.startswith("/") and len(
+                glob(source_root + '/' + treatment_day.strftime('%Y/%m/%d') + '/*.json.log*')) > 0:
+            statsLines = statsLines.union(
+                sc.textFile(source_root + '/' + treatment_day.strftime('%Y/%m/%d') + '/*.json.log*'))
+        treatment_day += timedelta(days=1)
+
+    statsLines = statsLines.map(
+        # json to dict
+        lambda stat: json.loads(stat)
+    )
+    return (sc, statsLines, treatment_day_start, treatment_day_end)
+
+def get_elapsed_time(start):
+    end = time()
+    runtime = end - start
+
+    minutes = int(math.floor(runtime / 60))
+    seconds = int(round(runtime - (minutes * 60)))
+    return (minutes, seconds)
+
+def terminate(sc):
+    sc.stop()
+
+def get_db_connection_string():
+    return "host='localhost' port='5432' dbname='statistics' user='statistics'"
+    # return "host='par-vm147.srv.canaltp.fr' port='5432' dbname='statistics' user='statistics' password='aitivan'"
+
+def log_analyzer_stats(analyzer, treatment_day_start, treatment_day_end, start_time):
+    if treatment_day_start == treatment_day_end:
+        (minutes, seconds) = get_elapsed_time(start_time)
+        print(
+            "[spark-stat-analyzer] [OK] [%s] [%s] [%s] [%dm%ds]" %
+            (datetime.now().strftime("%Y-%m-%d %H:%M:%S"), treatment_day_start, analyzer, minutes, seconds)
+        )
+
+def insert_data_into_db(table_name, columns, rows):
+    conn = psycopg2.connect(get_db_connection_string())
+    cur = conn.cursor()
+
+    columns_as_string = ", ".join(columns)
+    records_list_template = ','.join(['%s'] * len(rows))
+    insertString = "INSERT INTO stat_compiled.{0} ({1}) VALUES {2}".format(
+        table_name, columns_as_string, records_list_template
+    )
+    cur.execute(insertString, rows)
+    cur.close()
+    conn.commit()
+    conn.close()
---
+++ users.py
@@ -0,0 +1,66 @@
+import sys, os, json
+sys.path.append(os.path.abspath("includes"))
+from datetime import datetime
+from time import time
+from includes import common
+import psycopg2
+
+start = time()
+(sc, new_users, treatment_day_start, treatment_day_end) = common.get_data_from_input(__file__)
+
+conn = psycopg2.connect(common.get_db_connection_string())
+cur = conn.cursor()
+cur.execute("SELECT id FROM stat_compiled.users")
+users_ids = tuple(item[0] for item in cur.fetchall())
+
+cur.close()
+conn.commit()
+conn.close()
+
+# print(users_ids)
+
+new_users = new_users.map(
+    # keep usefull data
+    lambda dict: (dict['user_id'], dict['user_name'], dict['request_date'])
+).filter(
+    # remove users already in database
+    lambda (user_id, user_name, request_date), users_ids=users_ids: user_id not in users_ids
+).map(
+    # format to reduce by key
+    lambda (user_id, user_name, request_date): (user_id, (user_name, request_date))
+)
+
+# take oldest date of new user
+new_users_oldest_date = new_users.reduceByKey(
+    lambda a, b: a if a[1] < b[1] else b
+)
+
+# take newest date of new user
+new_users_newest_name = new_users.reduceByKey(
+    lambda a, b: a if a[1] > b[1] else b
+)
+
+# print('=========================')
+# print(new_users_newest_name.count())
+# print(new_users_newest_name.collect())
+# print('=========================')
+
+
+new_users_newest_name_collected = new_users_newest_name.collect()
+new_users_oldest_date_collected = new_users_oldest_date.collect()
+
+common.terminate(sc)
+
+if len(new_users_newest_name_collected) != 0 and len(new_users_oldest_date_collected) != 0:
+    # combine for insertion
+    rows = []
+    for (id_oldest, (name_oldest, timestamp_oldest)) in new_users_oldest_date_collected:
+        for (id_newest, (name_newest, timestamp_newest)) in new_users_newest_name_collected:
+            if id_oldest == id_newest:
+                rows.append((id_oldest, name_newest, datetime.fromtimestamp(timestamp_oldest)))
+    columns = ("id", "user_name", "date_first_request")
+    common.insert_data_into_db("users", columns, rows)
+    # print(insertString)
+    # print(rows)
+
+common.log_analyzer_stats("CanalTP\StatCompiler\Updater\UsersUpdater", treatment_day_start, treatment_day_end, start)
