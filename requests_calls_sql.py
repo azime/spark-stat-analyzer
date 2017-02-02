acdf1118b1c185cf33514a494c4f61f6c65287ba
dno <david.negrello@canaltp.fr>
2017-02-02 17:39:08 +0100

    useless removed

--- requests_calls_sql.py
+++ requests_calls_sql.py
@@ -2,7 +2,6 @@ import sys, os
 sys.path.append(os.path.abspath("includes"))
 from time import time
 from includes import common
-import psycopg2
 from pyspark.sql.functions import when, from_unixtime, lit
 
 start = time()
