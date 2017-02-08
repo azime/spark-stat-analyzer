253476d48bb38d7ec8352b208756cd3d17be48fc
Nicolas Berard <nicolas.berard@canaltp.fr>
2017-02-08 14:41:27 +0100

    add dockerfile for tests and bootstrap it on token_stat

--- token_stat.py
+++ token_stat.py
@@ -4,24 +4,27 @@ from time import time
 from includes import common
 from pyspark.sql.functions import to_date
 
-start = time()
+def process(df):
+    dfProcessed = df.withColumn('request_date_ts', df.request_date.cast('timestamp'))
+    tokenStats = dfProcessed.groupBy(to_date('request_date_ts').alias('request_date_trunc'), 'token').count()
+    tokenStats = tokenStats.collect()
+    # tokenRow attributes can be accessed by .token, .request_date_trunc but not .count which returns count method of tuple
+    return [(tokenRow['token'], tokenRow['request_date_trunc'], tokenRow['count']) for tokenRow in tokenStats]
 
-(source_root, treatment_day_start, treatment_day_end) = common.get_period_from_input()
-spark = common.start_spark_session(__file__)
-file_list = common.get_file_list(source_root, treatment_day_start, treatment_day_end)
+if __name__ == "__main__":
 
-df = common.get_sql_data_frame(spark, file_list)
+    start = time()
 
-dfProcessed = df.withColumn('request_date_ts', df.request_date.cast('timestamp'))
-tokenStats = dfProcessed.groupBy(to_date('request_date_ts').alias('request_date_trunc'), 'token').count()
-tokenStats = tokenStats.collect()
+    (source_root, treatment_day_start, treatment_day_end) = common.get_period_from_input()
+    spark = common.start_spark_session(__file__)
+    file_list = common.get_file_list(source_root, treatment_day_start, treatment_day_end)
 
-# tokenRow attributes can be accessed by .token, .request_date_trunc but not .count which returns count method of tuple
-tokenStats = [(tokenRow['token'], tokenRow['request_date_trunc'], tokenRow['count']) for tokenRow in tokenStats]
+    df = common.get_sql_data_frame(spark, file_list)
+    tokenStats = process(df)
 
-if len(tokenStats) != 0:
-    conn = common.truncate_table_for_dates("token_stats", (treatment_day_start, treatment_day_end))
-    common.insert_data_into_db("token_stats", ["token", "request_date", "nb_req"], tokenStats, conn)
+    if len(tokenStats) != 0:
+        conn = common.truncate_table_for_dates("token_stats", (treatment_day_start, treatment_day_end))
+        common.insert_data_into_db("token_stats", ["token", "request_date", "nb_req"], tokenStats, conn)
 
-common.terminate(spark.sparkContext)
-common.log_analyzer_stats("CanalTP\StatCompiler\Updater\TokenStatsUpdater", treatment_day_start, treatment_day_end, start)
+    common.terminate(spark.sparkContext)
+    common.log_analyzer_stats("TokenStatsUpdater", treatment_day_start, treatment_day_end, start)
