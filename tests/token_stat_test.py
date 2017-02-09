5721c308a86e932c39f0cd49d41b3ed55523ea40
Abderrahim Azime <abderrahim.azime@canaltp.fr>
2017-02-09 12:09:21 +0100

    file name modified

--- tests/token_stat_test.py
+++ tests/token_stat_test.py
@@ -5,13 +5,15 @@ import token_stat
 
 pytestmark = pytest.mark.usefixtures("spark")
 
+
 def test_token_stat(spark):
-    file_list = 'tests/fixtures/token_stat.json'
+    file_list = 'tests/fixtures/token_stat.log'
     df = common.get_sql_data_frame(spark, file_list)
     results = token_stat.process(df)
-    expected_results = [(u'da821360-5fdc-47cb-acc3-9025e56f0003', date(2017, 1, 15), 6),
-                        (u'da821360-5fdc-47cb-acc3-9025e56f0001', date(2017, 1, 15), 2),
-                        (u'da821360-5fdc-47cb-acc3-9025e56f0002', date(2017, 1, 16), 1),
-                        (u'da821360-5fdc-47cb-acc3-9025e56f0002', date(2017, 1, 15), 1),
-                        (u'da821360-5fdc-47cb-acc3-9025e56f0001', date(2017, 1, 16), 2)]
-    assert results == expected_results
+    expected_results = [(u'token:3', date(2017, 1, 15), 6),
+                        (u'token:1', date(2017, 1, 15), 2),
+                        (u'token:2', date(2017, 1, 16), 1),
+                        (u'token:2', date(2017, 1, 15), 1),
+                        (u'token:1', date(2017, 1, 16), 2)]
+    assert len(results) == len(expected_results)
+    assert len(set(results) - set(expected_results)) == 0
