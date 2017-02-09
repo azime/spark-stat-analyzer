1da123d81b9e638f67f5dd8a03f9782d1eb25ae2
Abderrahim Azime <abderrahim.azime@canaltp.fr>
2017-02-09 15:32:58 +0100

    correction

--- tests/token_stat_test.py
+++ tests/token_stat_test.py
@@ -7,13 +7,13 @@ pytestmark = pytest.mark.usefixtures("spark")
 
 
 def test_token_stat(spark):
-    file_list = 'tests/fixtures/token_stat.log'
+    file_list = 'tests/fixtures/token_stat.json.log'
     df = common.get_sql_data_frame(spark, file_list)
     results = token_stat.process(df)
-    expected_results = [(u'token:3', date(2017, 1, 15), 6),
-                        (u'token:1', date(2017, 1, 15), 2),
+    expected_results = [(u'token:2', date(2017, 1, 15), 1),
                         (u'token:2', date(2017, 1, 16), 1),
-                        (u'token:2', date(2017, 1, 15), 1),
+                        (u'token:3', date(2017, 1, 15), 6),
+                        (u'token:1', date(2017, 1, 15), 2),
                         (u'token:1', date(2017, 1, 16), 2)]
     assert len(results) == len(expected_results)
-    assert len(set(results) - set(expected_results)) == 0
+    assert results == expected_results
