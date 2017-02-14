2b78c665762d3b440700713dbddb080a07c71eaf
Abderrahim Azime <abderrahim.azime@canaltp.fr>
2017-02-14 11:45:52 +0100

    tests added

--- tests/database_test.py
+++ tests/database_test.py
@@ -10,3 +10,10 @@ def test_params_database():
     d = fixture_database()
     assert d.connection_string == "host='localhost' port='5432' dbname='dbname' user='user' password='password'"
     assert d.schema == "sh"
+
+
+def test_format_insert_query():
+    columns = ["c1", "c2"]
+    data = [("a", "b"), ("c", "d")]
+    d = fixture_database()
+    assert d.format_insert_query("table1", columns, data) == "INSERT INTO sh.table1 (c1, c2) VALUES %s,%s"
