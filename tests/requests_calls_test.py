aa4ec8bfb6782a7685265887c70c81ffb3b54224
Nicolas Berard <nicolas.berard@canaltp.fr>
2017-02-20 14:29:31 +0100

    fix existing tests failed because of refactoring

--- tests/requests_calls_test.py
+++ tests/requests_calls_test.py
@@ -15,7 +15,7 @@ def test_requests_calls(spark):
     analyze_request = AnalyzeRequest(storage_path=path,
                                      start_date=start_date,
                                      end_date=end_date,
-                                     spark_context=spark,
+                                     spark_session=spark,
                                      database=None,
                                      current_datetime=datetime(2017, 2, 15, 15, 10))
 
@@ -45,7 +45,7 @@ def test_requests_calls_without_journeys(spark):
     analyze_request = AnalyzeRequest(storage_path=path,
                                      start_date=start_date,
                                      end_date=end_date,
-                                     spark_context=spark,
+                                     spark_session=spark,
                                      database=None)
 
     files = analyze_request.get_files_to_analyze()
