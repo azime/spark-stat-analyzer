9a6758218599a723b0554adfe09d985070ee3029
Abderrahim Azime <abderrahim.azime@canaltp.fr>
2017-02-27 17:42:51 +0100

    corrections tests

--- tests/coverage_modes_test.py
+++ tests/coverage_modes_test.py
@@ -10,25 +10,25 @@ pytestmark = pytest.mark.usefixtures("spark")
 def test_get_tuples_from_stat_dict_without_journey():
     data = {"a": 1, "b": 2}
     result = AnalyzeCoverageModes.get_tuples_from_stat_dict(data)
-    assert result == []
+    assert [v for v in result] == []
 
 
 def test_get_tuples_from_stat_dict_with_empty_journeys():
     data = {"a": 1, "journeys": []}
     result = AnalyzeCoverageModes.get_tuples_from_stat_dict(data)
-    assert result == [v for v in result]
+    assert [v for v in result] == []
 
 
 def test_get_tuples_from_stat_dict_without_sections():
     data = {"a": 1, "journeys": [{"user_name": "bob", "token": "1234"}]}
     result = AnalyzeCoverageModes.get_tuples_from_stat_dict(data)
-    assert result == [v for v in result]
+    assert [v for v in result] == []
 
 
 def test_get_tuples_from_stat_dict_without_coverage():
     data = {"a": 1, "journeys": [{"user_name": "bob", "token": "1234"}]}
     result = AnalyzeCoverageModes.get_tuples_from_stat_dict(data)
-    assert result == [v for v in result]
+    assert [v for v in result] == []
 
 
 def test_coverage_modes_count(spark):
