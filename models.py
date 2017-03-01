395241243d9e04e2bc54436fbcda414d0bb044d2
Abderrahim Azime <abderrahim.azime@canaltp.fr>
2017-03-01 18:53:54 +0100

    versionnaing database

---
+++ models.py
@@ -0,0 +1,24 @@
+from sqlalchemy import Column, Integer, Text, Date, BIGINT, UniqueConstraint
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import MetaData
+
+Base = declarative_base(metadata=MetaData(schema='stat_compiled'))
+
+
+class CoverageStartEndNetwork(Base):
+    __tablename__ = 'coverage_start_end_network'
+
+    region_id = Column(Text(), primary_key=True,  nullable=False)
+    start_network_id = Column(Text(), primary_key=True,  nullable=False)
+    start_network_name = Column(Text(), primary_key=False,  nullable=False)
+    end_network_id = Column(Text(), primary_key=True,  nullable=False)
+    end_network_name = Column(Text(), primary_key=False,  nullable=False)
+    request_date = Column(Date(), primary_key=True,  nullable=False)
+    is_internal_call = Column(Integer(), primary_key=True,  nullable=False)
+    nb = Column(BIGINT(), primary_key=False,  nullable=False)
+    __table_args__ = (
+        UniqueConstraint(
+            'region_id', 'start_network_id', 'end_network_id', 'request_date', 'is_internal_call',
+            name='coverage_start_end_networks_pkey'
+        ),
+    )
