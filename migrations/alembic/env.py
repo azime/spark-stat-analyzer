74ec1bea32f4f9e124b9a87ebea3b7cd3d39e8e4
Nicolas Berard <nicolas.berard@canaltp.fr>
2017-03-02 15:45:32 +0100

    inherit alembic parameters from config.py

--- migrations/alembic/env.py
+++ migrations/alembic/env.py
@@ -1,38 +1,26 @@
 from __future__ import with_statement
 from alembic import context
 from sqlalchemy import engine_from_config, pool
-from logging.config import fileConfig
 from models import Base
-# this is the Alembic Config object, which provides
-# access to the values within the .ini file in use.
-config = context.config
-
-# Interpret the config file for Python logging.
-# This line sets up loggers basically.
-fileConfig(config.config_file_name)
 
 # add your model's MetaData object here
 # for 'autogenerate' support
 target_metadata = Base.metadata
 
+
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
-
+import config
 
 def get_url():
-    return config.get_main_option("sqlalchemy.url")
-    """
-    TODO manage env variables
-    import os
     return "postgresql://%s:%s@%s/%s" % (
-        os.getenv("DB_USER", "navitia"),
-        os.getenv("DB_PASSWORD", "navitia"),
-        os.getenv("DB_HOST", "navitia"),
-        os.getenv("DB_NAME", "navitia"),
+        config.db['user'],
+        config.db['password'],
+        config.db['host'],
+        config.db['dbname'],
     )
-    """
 
 
 def run_migrations_offline():
@@ -60,22 +48,25 @@ def run_migrations_online():
     and associate a connection with the context.
 
     """
+
     engine = engine_from_config(
-                config.get_section(config.config_ini_section),
-                prefix='sqlalchemy.',
-                poolclass=pool.NullPool)
+        {'sqlalchemy.url': get_url(),
+         'here': '', 'script_location': 'alembic'},
+        prefix='sqlalchemy.',
+        poolclass=pool.NullPool)
 
     connection = engine.connect()
     context.configure(
-                connection=connection,
-                target_metadata=target_metadata
-                )
+        connection=connection,
+        target_metadata=target_metadata
+    )
 
     try:
         with context.begin_transaction():
             context.run_migrations()
     finally:
         connection.close()
+
 
 if context.is_offline_mode():
     run_migrations_offline()
