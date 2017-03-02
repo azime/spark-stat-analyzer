0f3d4575f9ca3b4aa98ba3c2c41caa50e28f0ff1
Abderrahim Azime <abderrahim.azime@canaltp.fr>
2017-03-02 11:42:24 +0100

    users table added

--- migrations/alembic/versions/37163dfe2f20_coverage_start_end_networks.py
+++ migrations/alembic/versions/37163dfe2f20_coverage_start_end_networks.py
@@ -14,19 +14,34 @@ import sqlalchemy as sa
 
 
 def upgrade():
-    op.create_table('coverage_start_end_network',
-    sa.Column('region_id', sa.Text(), nullable=False),
-    sa.Column('start_network_id', sa.Text(), nullable=False),
-    sa.Column('start_network_name', sa.Text(), nullable=False),
-    sa.Column('end_network_id', sa.Text(), nullable=False),
-    sa.Column('end_network_name', sa.Text(), nullable=False),
-    sa.Column('request_date', sa.Date(), nullable=False),
-    sa.Column('is_internal_call', sa.Integer(), nullable=False),
-    sa.Column('nb', sa.BIGINT(), nullable=False),
-    sa.PrimaryKeyConstraint('region_id', 'start_network_id', 'end_network_id', 'request_date', 'is_internal_call'),
-    sa.UniqueConstraint('region_id', 'start_network_id', 'end_network_id', 'request_date', 'is_internal_call', name='coverage_start_end_networks_pkey'),
-    schema='stat_compiled'
+    op.create_table(
+        'coverage_start_end_network',
+        sa.Column('region_id', sa.Text(), nullable=False),
+        sa.Column('start_network_id', sa.Text(), nullable=False),
+        sa.Column('start_network_name', sa.Text(), nullable=False),
+        sa.Column('end_network_id', sa.Text(), nullable=False),
+        sa.Column('end_network_name', sa.Text(), nullable=False),
+        sa.Column('request_date', sa.Date(), nullable=False),
+        sa.Column('is_internal_call', sa.Integer(), nullable=False),
+        sa.Column('nb', sa.BIGINT(), nullable=False),
+        sa.PrimaryKeyConstraint('region_id', 'start_network_id', 'end_network_id', 'request_date', 'is_internal_call'),
+        sa.UniqueConstraint('region_id', 'start_network_id', 'end_network_id', 'request_date', 'is_internal_call',
+                            name='coverage_start_end_networks_pkey'),
+        schema='stat_compiled'
     )
+    context = op.get_context()
+    connection = op.get_bind()
+
+    if not context.dialect.has_table(connection.engine, table_name='users', schema="stat_compiled"):
+        op.create_table(
+            'users',
+            sa.Column('id', sa.Text(), nullable=False),
+            sa.Column('user_name', sa.Text(), nullable=True),
+            sa.Column('date_first_request', sa.DateTime(), nullable=True),
+            sa.PrimaryKeyConstraint('id'),
+            sa.UniqueConstraint('id', name='users_pkey'),
+            schema='stat_compiled'
+        )
 
 
 def downgrade():
--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, Text, Date, BIGINT, UniqueConstraint
+from sqlalchemy import Column, Integer, Text, Date, BIGINT, UniqueConstraint, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import MetaData
 
@@ -20,5 +20,18 @@ class CoverageStartEndNetwork(Base):
         UniqueConstraint(
             'region_id', 'start_network_id', 'end_network_id', 'request_date', 'is_internal_call',
             name='coverage_start_end_networks_pkey'
+        ),
+    )
+
+
+class Users(Base):
+    __tablename__ = 'users'
+
+    id = Column(Text(), primary_key=True,  nullable=False)
+    user_name = Column(Text(), primary_key=False,  nullable=True)
+    date_first_request = Column(DateTime(), primary_key=False,  nullable=True)
+    __table_args__ = (
+        UniqueConstraint(
+            'id', name='users_pkey'
         ),
     )
