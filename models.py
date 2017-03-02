332c6da7f1aa01a8bb9116fc2d74f47f3dabbde9
Abderrahim Azime <abderrahim.azime@canaltp.fr>
2017-03-02 16:20:57 +0100

    coverage_start_end_networks table added

--- models.py
+++ models.py
@@ -5,7 +5,7 @@ from sqlalchemy import MetaData
 Base = declarative_base(metadata=MetaData(schema='stat_compiled'))
 
 
-class CoverageStartEndNetwork(Base):
+class CoverageStartEndNetworks(Base):
     __tablename__ = 'coverage_start_end_networks'
 
     region_id = Column(Text(), primary_key=True,  nullable=False)
@@ -13,9 +13,9 @@ class CoverageStartEndNetwork(Base):
     start_network_name = Column(Text(), primary_key=False,  nullable=False)
     end_network_id = Column(Text(), primary_key=True,  nullable=False)
     end_network_name = Column(Text(), primary_key=False,  nullable=False)
-    request_date = Column(Date(), primary_key=True,  nullable=False)
-    is_internal_call = Column(Integer(), primary_key=True,  nullable=False)
-    nb = Column(BIGINT(), primary_key=False,  nullable=False)
+    request_date = Column(DateTime(), primary_key=True,  nullable=False)
+    is_internal_call = Column(Boolean(), primary_key=True,  nullable=False)
+    nb = Column(BigInteger(), primary_key=False,  nullable=False)
     __table_args__ = (
         UniqueConstraint(
             'region_id', 'start_network_id', 'end_network_id', 'request_date', 'is_internal_call',
@@ -205,14 +205,3 @@ class TokenStats(Base):
         ),
     )
 
-class Users(Base):
-    __tablename__ = 'users'
-
-    id = Column(Text(), primary_key=True, nullable=False)
-    user_name = Column(Text(), primary_key=False, nullable=True)
-    date_first_request = Column(DateTime(), primary_key=False, nullable=True)
-    __table_args__ = (
-        UniqueConstraint(
-            'id', name='users_pkey'
-        ),
-    )
