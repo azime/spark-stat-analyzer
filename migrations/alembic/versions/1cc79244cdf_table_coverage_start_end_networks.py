bb6e33437ff8b9ec27ce630b793f3fdba8ffa187
Abderrahim Azime <abderrahim.azime@canaltp.fr>
2017-03-03 15:34:47 +0100

    create trigger

--- migrations/alembic/versions/1cc79244cdf_table_coverage_start_end_networks.py
+++ migrations/alembic/versions/1cc79244cdf_table_coverage_start_end_networks.py
@@ -14,10 +14,12 @@ from alembic import op
 import sqlalchemy as sa
 import config
 
+table = "coverage_start_end_networks"
+
 
 def upgrade():
     op.create_table(
-        'coverage_start_end_networks',
+        table,
         sa.Column('region_id', sa.Text(), nullable=False),
         sa.Column('start_network_id', sa.Text(), nullable=False),
         sa.Column('start_network_name', sa.Text(), nullable=False),
@@ -28,10 +30,41 @@ def upgrade():
         sa.Column('nb', sa.BigInteger(), nullable=False),
         sa.PrimaryKeyConstraint('region_id', 'start_network_id', 'end_network_id', 'request_date', 'is_internal_call'),
         sa.UniqueConstraint('region_id', 'start_network_id', 'end_network_id', 'request_date', 'is_internal_call',
-                            name='{schema}_coverage_start_end_networks_pkey'.format(schema=config.db['schema'])),
+                            name='{schema}_{table}_pkey'.format(schema=config.db['schema'], table=table)),
         schema=config.db['schema']
     )
+    op.execute("""
+            CREATE OR REPLACE FUNCTION {table}_insert_trigger()
+                RETURNS TRIGGER AS $$
+                DECLARE
+                  schema VARCHAR(100);
+                  partition VARCHAR(100);
+                BEGIN
+                  schema := '{schema}';
+                  partition := '{table}' || '_' || to_char(NEW.request_date, '"y"YYYY"m"MM');
+                  IF NOT EXISTS(SELECT 1 FROM pg_tables WHERE tablename=partition and schemaname=schema) THEN
+                    RAISE NOTICE 'A partition has been created %',partition;
+                    EXECUTE 'CREATE TABLE IF NOT EXISTS ' || schema || '.' || partition ||
+                            ' (
+                              check (request_date >= DATE ''' || to_char(NEW.request_date, 'YYYY-MM-01') || '''
+                                      AND request_date < DATE ''' || to_char(NEW.request_date + interval '1 month', 'YYYY-MM-01') || ''') ) ' ||
+                            'INHERITS (' || schema || '.{table});';
+                  END IF;
+                  EXECUTE 'INSERT INTO ' || schema || '.' || partition || ' SELECT(' || schema || '.{table}' || ' ' || quote_literal(NEW) || ').*;';
+                  RETURN NULL;
+                END;
+                $$
+                LANGUAGE plpgsql;
+            """.format(schema=config.db['schema'], table=table
+        ))
+    op.execute("""
+            CREATE TRIGGER insert_{table}_trigger
+                BEFORE INSERT ON {schema}.{table}
+                FOR EACH ROW EXECUTE PROCEDURE {table}_insert_trigger();
+            """.format(table=table, schema=config.db['schema']
+        ))
 
 
 def downgrade():
-    op.drop_table('coverage_start_end_networks', schema=config.db['schema'])
+    op.drop_table(table, schema=config.db['schema'])
+    op.execute("""DROP FUNCTION IF EXISTS {table}_insert_trigger();""".format(table=table))
