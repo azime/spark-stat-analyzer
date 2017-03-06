dae213e5252fc4ac02cfd398ec1cb7d9b8650b4d
Rémy Abi-Khalil <remyabi@gmail.com>
2017-03-06 18:11:19 +0100

    Users table updated. Now first request date saved in database

--- tests/users_sql_test.py
+++ tests/users_sql_test.py
@@ -23,11 +23,11 @@ def test_users_sql(spark):
 
     results = analyzer.get_data()
 
-    expected_results = {666: {'last_user_name': u'Billy', 'first_date': 1484467930},
-                        42: {'last_user_name': u'Kenny last name', 'first_date': 1484459770},
-                        15: {'last_user_name': u'Bobby new name', 'first_date': 1484467930}}
+    expected_results = {666: {'user_name': u'Billy', 'first_date': 1484467930},
+                        42: {'user_name': u'Kenny last name', 'first_date': 1484459770},
+                        15: {'user_name': u'Bobby new name', 'first_date': 1484467930}}
     assert len(results) == len(expected_results)
-    formatted_results = {elt.user_id: {"last_user_name": elt.last_user_name, "first_date": elt.first_date}
+    formatted_results = {elt.user_id: {"user_name": elt.user_name, "first_date": elt.first_date}
                          for elt in results}
     for row_id, row in formatted_results.items():
         assert row == expected_results[row_id]
